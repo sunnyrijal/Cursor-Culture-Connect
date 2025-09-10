5ace7de0158b45c8d3e5c7ba2b7632c6478e9229
babish9887 <babishchaudhary.dev@gmail.com>
2025-09-10 22:06:24 +0545

    chore: Changed Backend URL

--- contexts/axiosConfig.ts
+++ contexts/axiosConfig.ts
@@ -4,8 +4,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 // export const API_URL = "https://4bhw4zcd-4000.inc1.devtunnels.ms/api"; 
 // export const API_BASE_URL = "https://4bhw4zcd-4000.inc1.devtunnels.ms"; 
 
-export const API_URL = "https://secureapi.trivoconnect.com/api"; 
-export const API_BASE_URL = "https://secureapi.trivoconnect.com"; 
+export const API_URL = "https://culture-connect-be.onrender.com/api"; 
+export const API_BASE_URL = "https://culture-connect-be.onrender.com"; 
 
 
 const api = axios.create({
